c3159170f12e62e5e9555638cd34e930ba28d957
Florence Loy <florencelry@gmail.com>
2020-09-17 15:19:08 +0800

    completed mentor sign up form and coded mentor functionality into profile page

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,10 +1,34 @@
 class MentorsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
+  skip_after_action :verify_authorized, only: [:new]
 
   def show
     @mentor = Mentor.find(params[:id])
     @user = @mentor.user
     @booking = Booking.new 
     @tab = params[:tab]
+  end
+
+  def new
+    @mentor = Mentor.new
+  end
+
+  def create 
+    @mentor = Mentor.new(mentor_params)
+    @mentor.user_id = current_user.id
+    authorize @mentor
+    @mentor.save 
+
+    if @mentor.save 
+      redirect_to myprofile_path(current_user), notice: "You're a mentor now!"
+    else 
+      redirect_to :back, alert: "Something went wrong!"
+    end
+  end 
+
+  private 
+
+  def mentor_params
+    params.require(:mentor).permit(:hourly_rate) 
   end
 end
