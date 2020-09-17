228d2921ea27c50acd3584099426bc2b5da1749b
yasmin diyana <yasmindyana@gmail.com>
2020-09-17 13:56:46 +0800

    renamed table and added testimonial column

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :mentors, only: [:show] do 
+  resources :mentors, only: [:show] do
     resources :bookings, only: [:create]
   end
   resources :users, only: [:show, :edit, :update, :destroy]
-
+  resources :reviews, only: [:new, :create]
   get 'myprofile', to: 'users#myprofile'
   post 'filter', to: 'pages#filter'
 end
+
